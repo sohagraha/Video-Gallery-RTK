2a8e7e844b8773d6809e3e543f6e7243da832ff8
sohagraha <sohagrahabd@gmail.com>
2022-09-10 10:11:05 +0600

    feat : initial setup for RTK

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+
 });
