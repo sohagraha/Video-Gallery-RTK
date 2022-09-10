e749c3df3f5a824dc9f3d9da4c004a0de1ff8a2f
sohagraha <sohagrahabd@gmail.com>
2022-09-10 16:49:25 +0600

    feat : Get query added to get all videos

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,34 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
 import Video from "./Video";
 
 export default function Videos() {
+    const { data: videos, isLoading, isError } = useGetVideosQuery();
+    let content = null;
+    if (isLoading) {
+        content = <>
+            <VideoLoader /><VideoLoader />
+            <VideoLoader /><VideoLoader />
+        </>
+    }
+    else if (!isLoading && isError) {
+        content = <Error message='There was an error' />
+    }
+    else if (!isLoading && !isError && videos.length === 0) {
+        content = <Error message='No videos found' />
+    }
+    else if (!isLoading && !isError && videos.length > 0) {
+        content = videos.map((video) => (
+            <Video key={video.id} video={video} />
+        ))
+    }
     return (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+            {
+                content
+            }
+
         </>
     );
 }
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -6,7 +6,13 @@ export const apiSlice = createApi({
         baseUrl: 'http://localhost:9000',
     }),
     endpoints: (builder) => ({
-    }),
+        getVideos: builder.query({
+            query: () => '/videos',
+        }),
+
+    })
 })
 
-
+export const {
+    useGetVideosQuery,
+} = apiSlice
